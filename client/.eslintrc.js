107d6a9f1e7a575a06b907228d11762ed895e899
zekeFurry <ezekielarcilla618@gmail.com>
2023-09-10 12:50:37 +0800

    Your commit message

---
+++ client/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // Specify the environments where your code will run
+    env: {
+      browser: true, // for browser-based projects
+      node: true,    // for Node.js projects
+      jest: true,    // if you're using Jest for testing
+    },
+    // Define your ESLint rules here
+    rules: {
+      // Example rules:
+      "semi": ["error", "always"], // Enforce semicolons
+      "indent": ["error", 2],     // Use 2 spaces for indentation
+    },
+  };  
